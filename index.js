511b93c271d3635b14135f1c4aeb36d9246c00b4
Richard Klose <richard.klose@gmail.com>
2016-11-13 18:28:38 +0100

    A little bit of refactoring and nicer structure

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var metawear = require('node-metawear');
 var config = require('./config.json');
-var initDevice = require('./lib/initDevice');
+var Device = require('./lib/Device');
+var device;
 
-metawear.discover( (device) => {
-    console.log('discovered device ', device.id);
-    for (var i in config.devices) {
-        if ( config.devices[i] === device.id || config.devices[i] === device.address )
-        initDevice(device);
+metawear.discover( (dev) => {
+    console.log('discovered device ', dev.id);
+    if ( config.device === dev.id || config.device === dev.address ) {
+        device = new Device(dev);
     }
 });
