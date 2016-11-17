8cc2bad3a88cba04ad3704748de8439e23ae62dc
Richard Klose <richard.klose@gmail.com>
2016-11-17 18:51:49 +0100

    Added basic mqtt features

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var metawear = require('node-metawear');
 var config = require('./config.json');
 var Device = require('./lib/Device');
+var mqttoptions
+try {
+    mqttoptions = require('./config/mqtt.json')
+} catch(e) {
+    mqttoptions = require('./config/mqtt.default.json')
+}
+var mqtt = require('mqtt').connect(mqttoptions);
+
+mqtt.on('connect', () => {
+    console.log('MQTT connected');
+});
+
 var device;
 
 metawear.discover( (dev) => {
     console.log('discovered device ', dev.id);
     if ( config.device === dev.id || config.device === dev.address ) {
         device = new Device(dev);
+
+        device.on('battery-status', (level) => {
+            mqtt.publish('MetaWearDice/Battery',level.toString());
+        });
     }
 });
