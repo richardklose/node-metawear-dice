82da82a870015fbeebb3c26d2b4f7ebb70506514
Richard Klose <richard.klose@gmail.com>
2016-11-20 19:18:27 +0100

    Added detection and MQTT messages for gyroscope

--- index.js
+++ index.js
@@ -10,18 +10,52 @@ try {
 var mqtt = require('mqtt').connect(mqttoptions);
 
 mqtt.on('connect', () => {
+    mqtt.subscribe('MetaWearDice/to/#');
     console.log('MQTT connected');
 });
 
-var device;
+function Movement(axis, value) {
+    if (value > 100) {
+        mqtt.publish('MetaWearDice/from/Gyro', axis + '-right');
+    } else if(value < -100) {
+        mqtt.publish('MetaWearDice/from/Gyro', axis + '-left');
+    }
+}
 
+var device;
+var value = 100;
 metawear.discover( (dev) => {
     console.log('discovered device ', dev.id);
     if ( config.device === dev.id || config.device === dev.address ) {
         device = new Device(dev);
 
         device.on('battery-status', (level) => {
-            mqtt.publish('MetaWearDice/Battery',level.toString());
+            mqtt.publish('MetaWearDice/from/Battery',level.toString());
         });
+
+        device.on('gyro-action', (sx,sy,sz) => {
+            var x = parseInt(sx);
+            var y = parseInt(sy);
+            var z = parseInt(sz);
+
+            var ax = Math.abs(x);
+            var ay = Math.abs(y);
+            var az = Math.abs(z);
+
+            if(ax > ay && ax > az) {
+                // x-Axis-Movement
+                Movement('x', x);
+            } else if(ay > ax && ay > az) {
+                // y-Axis-Movement
+                Movement('y', y);
+            } else if(az > ay && az > ax) {
+                // z-Axis-Movement
+                Movement('z', z);
+            }
+        });
+
+        device.init();
     }
 });
+
+
